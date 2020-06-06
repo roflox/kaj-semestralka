6f658a2dbe15a329225e395dbdf1c5e282e06353
roflox <wow.brog@gmail.com>
2020-06-06 11:37:25 +0200

    svg rework

--- src/renderer.ts
+++ src/renderer.ts
@@ -25,6 +25,9 @@ class Renderer {
     private secretValues: GetSecretDTO[] = [];
     private readonly themeSwitch = document.getElementById(
         "theme-switch") as HTMLInputElement;
+    private static readonly EYE_HTML = "<path fill=\"currentColor\" d=\"M572.52 241.4C518.29 135.59 410.93 64 288 64S57.68 135.64 3.48 241.41a32.35 32.35 0 0 0 0 29.19C57.71 376.41 165.07 448 288 448s230.32-71.64 284.52-177.41a32.35 32.35 0 0 0 0-29.19zM288 400a144 144 0 1 1 144-144 143.93 143.93 0 0 1-144 144zm0-240a95.31 95.31 0 0 0-25.31 3.79 47.85 47.85 0 0 1-66.9 66.9A95.78 95.78 0 1 0 288 160z\"></path>"
+    private static readonly EYE_SLASH_HTML = "<path fill=\"currentColor\" d=\"M320 400c-75.85 0-137.25-58.71-142.9-133.11L72.2 185.82c-13.79 17.3-26.48 35.59-36.72 55.59a32.35 32.35 0 0 0 0 29.19C89.71 376.41 197.07 448 320 448c26.91 0 52.87-4 77.89-10.46L346 397.39a144.13 144.13 0 0 1-26 2.61zm313.82 58.1l-110.55-85.44a331.25 331.25 0 0 0 81.25-102.07 32.35 32.35 0 0 0 0-29.19C550.29 135.59 442.93 64 320 64a308.15 308.15 0 0 0-147.32 37.7L45.46 3.37A16 16 0 0 0 23 6.18L3.37 31.45A16 16 0 0 0 6.18 53.9l588.36 454.73a16 16 0 0 0 22.46-2.81l19.64-25.27a16 16 0 0 0-2.82-22.45zm-183.72-142l-39.3-30.38A94.75 94.75 0 0 0 416 256a94.76 94.76 0 0 0-121.31-92.21A47.65 47.65 0 0 1 304 192a46.64 46.64 0 0 1-1.54 10l-73.61-56.89A142.31 142.31 0 0 1 320 112a143.92 143.92 0 0 1 144 144c0 21.63-5.29 41.79-13.9 60.11z\"></path>";
+
 
     private eyeListener = (event: Event) => {
         const target =
@@ -35,12 +38,15 @@ class Renderer {
         if (target.classList.contains("eye-slash")) {
             // je vyzadovano zadani hesla, popr se uz heslo zobrazilo
             target.classList.replace("eye-slash", "eye");
+            target.getElementsByTagName("svg").item(0).innerHTML=Renderer.EYE_HTML;
             parent.getElementsByTagName("input").item(0).style.display = "none";
             parent.getElementsByTagName("button").item(0).style.display = "none";
             const divs = parent.getElementsByTagName("div");
             divs.item(3).innerText = divs.item(4).innerText;
         } else {
             target.classList.replace("eye", "eye-slash");
+            // console.log(target.getElementsByTagName("svg").item(0));
+            target.getElementsByTagName("svg").item(0).innerHTML=Renderer.EYE_SLASH_HTML;
             parent.getElementsByTagName("input").item(0).style.display = "block";
             parent.getElementsByTagName("button").item(0).style.display = "block";
             // const divs = parent.getElementsByTagName("div");
@@ -113,6 +119,20 @@ class Renderer {
                 //iconButton + tooltip
                 const tooltip = document.createElement("span");
                 const iconButtonDiv = document.createElement("div");
+                const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg") as SVGElement;
+                //aria-hidden=\"true\" width=\"10%\" height=\"10%\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"eye\" class=\"svg-inline--fa fa-eye fa-w-18\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 576 512\">
+
+                svg.setAttribute("aria-hidden","true");
+                svg.setAttribute("width","16px");
+                svg.setAttribute("height","16px");
+                svg.setAttribute("focusable","false");
+                // svg.setAttribute("data-prefix","fas");
+                // svg.setAttribute("data-icon","eye");
+                // svg.setAttribute("role","img")
+                // svg.setAttribute("xmlns","http://www.w3.org/2000/svg");
+                svg.setAttribute("viewBox","0 0 576 512");
+                svg.innerHTML = Renderer.EYE_HTML;
+                iconButtonDiv.appendChild(svg);
                 iconButtonDiv.classList.add("eye");
                 iconButtonDiv.appendChild(tooltip);
                 iconButtonDiv.addEventListener("click", this.eyeListener);
